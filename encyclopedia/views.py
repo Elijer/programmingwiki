4b2796cdd4f413dc6ebfbabb2639927e3690e262
Jah <kennedye2@vcu.edu>
2021-07-11 18:42:16 +0200

    entry path now displays a rendered page for that entry

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -7,3 +7,9 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def entry(request, entry):
+    return render(request, "encyclopedia/entry.html", {
+        "content": util.get_entry(entry),
+        "title": entry.capitalize()
+    })
