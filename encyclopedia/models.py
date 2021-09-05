57fd52c4153a4231f89f1aec6c836455790deb0f
Jah <kennedye2@vcu.edu>
2021-09-05 09:22:38 -0500

    created model and added pipfile

--- encyclopedia/models.py
+++ encyclopedia/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Entry(models.Model):
+    title = models.CharField(max_length=32, unique=True)
+    body = models.TextField()
+
+    def __str__(self):
+        return f"Title: {self.title}"
